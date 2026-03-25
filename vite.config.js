d8a5c1901bf56ec0cd4b22504d74bcbcb8178ce4
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2026-03-25 22:05:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// i copied this from the docs lol
 export default defineConfig({
   plugins: [react()],
+  base: '/shop-app/',  // ← matches your repo name exactly
 })
